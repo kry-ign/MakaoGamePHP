1d7050bd0510e7bffe8fd32a682abf1bd52576a8
kry.ign93 <kris_ignatiuk@wp.pl>
2021-02-07 13:57:22 +0100

    test iterator

--- tests/Makao/Collection/CardCollectionTest.php
+++ tests/Makao/Collection/CardCollectionTest.php
@@ -6,6 +6,7 @@ namespace Tests\Makao\Collection;
 
 use Makao\Card;
 use Makao\Collection\CardCollection;
+use Makao\Exception\CardNotFoundExpection;
 use PHPUnit\Framework\TestCase;
 
 class CardCollectionTest extends TestCase
@@ -40,5 +41,44 @@ class CardCollectionTest extends TestCase
         //then
         $this->assertCount(1,$this->cartCollection);
 
+    }
+    public function testShouldAddNewCardsInChainToCardCollection(){
+        //expect
+
+        //given
+        $firstCard = new Card();
+        $secondCard = new Card();
+        //when
+        $this->cartCollection
+            ->add($firstCard)
+            ->add($secondCard);
+        //then
+        $this->assertCount(2,$this->cartCollection);
+    }
+    public function testShouldThrowCardNotFoundExpectionWhenITryPickCardFromEmptyCardCollection(){
+        //expect
+        $this->expectException(CardNotFoundExpection::class);
+        $this->expectExceptionMessage('You can not pick card form empty CradCollection!');
+        //given
+
+        //when
+        $this->cartCollection->pickCard();
+        //then
+    }
+    public function testShouldIterableOnCardCollection(){
+        //expect
+
+        //given
+        $card = new Card();
+        //when & then
+        $this->cartCollection->add($card);
+
+        $this->assertTrue($this->cartCollection->valid());
+        $this->assertSame($card, $this->cartCollection->current());
+        $this->assertSame(0, $this->cartCollection->key());
+
+        $this->cartCollection->next();
+        $this->assertFalse($this->cartCollection->valid());
+        $this->assertSame(1, $this->cartCollection->key());
     }
 }
